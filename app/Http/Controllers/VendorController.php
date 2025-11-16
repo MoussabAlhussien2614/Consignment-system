08a2020acfe6267ef3cebd1249a5da3189eaa043
MoussabAlhussien2614 <moussab2614@gmail.com>
2025-11-16 12:53:17 +0300

    adding vendor controller (index with search , show , update , store , delete)

--- app/Http/Controllers/VendorController.php
+++ app/Http/Controllers/VendorController.php
@@ -2,9 +2,78 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreVendorRequest;
+use App\Http\Requests\UpdateVendorRequest;
+use App\Models\Vendor;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class VendorController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        $query = Vendor::query()->withCount(['vehicles', 'consignments']);
+
+        if ($request->has('search') && $request->search) {
+            $search = $request->search;
+            $query->where(function ($q) use ($search) {
+                $q->where('name', 'like', "%{$search}%")
+                    ->orWhere('code', 'like', "%{$search}%")
+                    ->orWhere('email', 'like', "%{$search}%")
+                    ->orWhere('contact_name', 'like', "%{$search}%");
+            });
+        }
+
+        $vendors = $query->latest()->paginate(15)->withQueryString();
+
+        return Inertia::render('Vendors/Index', [
+            'vendors' => $vendors,
+            'filters' => $request->only(['search']),
+        ]);
+    }
+
+    public function create()
+    {
+        return Inertia::render('Vendors/Create');
+    }
+
+    public function store(StoreVendorRequest $request)
+    {
+        Vendor::create($request->validated());
+
+        return redirect()->route('vendors.index')
+            ->with('success', 'Vendor created successfully.');
+    }
+
+    public function show(Vendor $vendor)
+    {
+        $vendor->load(['vehicles', 'consignments']);
+
+        return Inertia::render('Vendors/Show', [
+            'vendor' => $vendor,
+        ]);
+    }
+
+    public function edit(Vendor $vendor)
+    {
+        return Inertia::render('Vendors/Edit', [
+            'vendor' => $vendor,
+        ]);
+    }
+
+    public function update(UpdateVendorRequest $request, Vendor $vendor)
+    {
+        $vendor->update($request->validated());
+
+        return redirect()->route('vendors.index')
+            ->with('success', 'Vendor updated successfully.');
+    }
+
+    public function destroy(Vendor $vendor)
+    {
+        $vendor->delete();
+
+        return redirect()->route('vendors.index')
+            ->with('success', 'Vendor deleted successfully.');
+    }
 }
