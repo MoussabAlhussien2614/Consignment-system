8e8430dc481b4f2d9e199c8d67ddd59d6e185c2a
Amro Baira <amr.baira@gmail.com>
2025-11-16 13:09:20 +0300

    add name to consignment

--- database/migrations/2025_11_15_120329_create_consignment_items_table.php
+++ database/migrations/2025_11_15_120329_create_consignment_items_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('consignment_items', function (Blueprint $table) {
             $table->id();
             $table->foreignId('consignment_id')->constrained()->cascadeOnDelete();
+            $table->string('name');
             $table->string('sku')->nullable()->unique();
             $table->foreignId('category_id')->constrained("categories")->cascadeOnDelete();
             $table->integer('quantity')->unsigned();
