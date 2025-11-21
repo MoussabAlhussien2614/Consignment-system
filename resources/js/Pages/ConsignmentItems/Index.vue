8b40de7bad2082ec7994779db72c1fa230f880b2
Amro Baira <amr.baira@gmail.com>
2025-11-21 20:32:32 +0300

    remove add button for consignment item

--- resources/js/Pages/ConsignmentItems/Index.vue
+++ resources/js/Pages/ConsignmentItems/Index.vue
@@ -31,15 +31,6 @@
               </Link>
             </div>
           </form>
-          <Link
-            :href="createRoute"
-            class="inline-flex items-center gap-2 px-4 py-2 bg-gradient-to-r from-indigo-600 to-purple-600 text-white rounded-lg hover:from-indigo-700 hover:to-purple-700 transition-all duration-200 shadow-md hover:shadow-lg whitespace-nowrap"
-          >
-            <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4" />
-            </svg>
-            Add Consignment
-          </Link>
         </div>
       </div>
         <ConsignmentItemTable
